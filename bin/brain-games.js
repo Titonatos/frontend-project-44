3dcd5a9d753783d485f234c4f66ba6749d868ef5
titonatos <titanium4ik777@gmail.com>
2023-10-08 00:03:56 +0300

    installed linter and fixed linter errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { greeting } from '../src/sli.js';
+import greeting from '../src/sli.js';
 
 greeting();
--- src/sli.js
+++ src/sli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    console.log("Welcome to the Brain Games!");
+export default () => {
+  console.log('Welcome to the Brain Games!');
 
-    const playerName = readlineSync.question('May I have your name? ');
-
-    console.log(`Hello ${playerName}`);
-}
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${playerName}`);
+};
